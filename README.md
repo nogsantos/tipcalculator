94ef097eee2cf7f59d64c056d8f6a9cfe117ea25
Fabricio Nogueira <nogsantos@gmail.com>
2015-09-23 16:41:03 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+#Estudos IOS
+
+Calculadora usando Swift 2.
+
+[Referência](http://www.raywenderlich.com/115279/swift-2-tutorial-part-2-a-simple-ios-app)
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var totalTextField  : UITextField!;
+    @IBOutlet var taxPctSlider    : UISlider!;
+    @IBOutlet var taxPctLabel     : UILabel!;
+    @IBOutlet var resultsTextView : UITextView!;
+    
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06);
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        refreshUI();
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +29,46 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func calculateTapped(sender : AnyObject) {
+        // 1
+        tipCalc.total = Double((totalTextField.text! as NSString).doubleValue)
+        // 2
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        // 3
+//        for (tipPct, tipValue) in possibleTips {
+//            // 4
+//            results += "\(tipPct)%: \(tipValue)\n"
+//        }
+        var keys = Array(possibleTips.keys)
+        keys.sortInPlace()
+        for tipPct in keys {
+            let tipValue = possibleTips[tipPct]!
+            let prettyTipValue = String(format:"%.2f", tipValue)
+            results += "\(tipPct)%: \(prettyTipValue)\n"
+        }
+        // 5
+        resultsTextView.text = results
+    }
+    @IBAction func taxPercentageChanged(sender : AnyObject) {
+        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        refreshUI()
+    }
+    @IBAction func viewTapped(sender : AnyObject) {
+        totalTextField.resignFirstResponder()
+    }
+    /**
+     * method to refresh the UI
+     */
+    func refreshUI() {
+        // 1
+        totalTextField.text = String(format: "%0.2f", tipCalc.total)
+        // 2
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        // 3
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        // 4
+        resultsTextView.text = ""
+    }
 }
 
